5985da8400b5f1b2e8f13c50aaaecf3a8a1bf0bf
Andrew Moore <me@andrewmoore.ca>
2014-06-01 18:47:24 -0400

    Fixed compatibility with 2.5 directory structure script

--- app/SymfonyStandard/RootPackageInstallSubscriber.php
+++ app/SymfonyStandard/RootPackageInstallSubscriber.php
@@ -15,9 +15,19 @@ use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\Script\ScriptEvents;
 use Composer\Script\CommandEvent;
 use Sensio\Bundle\DistributionBundle\Composer\ScriptHandler;
+use Symfony\Component\Filesystem\Filesystem;
 
 class RootPackageInstallSubscriber implements EventSubscriberInterface
 {
+    /**
+     * Composer variables are declared static so that an event could update
+     * a composer.json and set new options, making them immediately available
+     * to forthcoming listeners.
+     */
+    private static $options = array(
+        'symfony-web-dir' => 'web'
+    );
+
     public static function installAcmeDemoBundle(CommandEvent $event)
     {
         ScriptHandler::installAcmeDemoBundle($event);
@@ -25,7 +35,17 @@ class RootPackageInstallSubscriber implements EventSubscriberInterface
 
     public static function setupNewDirectoryStructure(CommandEvent $event)
     {
+        $options = self::getOptions($event);
+
+        // Create dummy app_dev.php to stay compatible with Symfony's post install file
+        $fs = new Filesystem();
+        $webDir = $options['symfony-web-dir'];
+        $fs->dumpFile($webDir.'/app_dev.php', '');
+
         ScriptHandler::defineDirectoryStructure($event);
+
+        // Remove the dummy app_dev.php file
+        $fs->remove($webDir.'/app_dev.php');
     }
 
     public static function getSubscribedEvents()
@@ -36,5 +56,12 @@ class RootPackageInstallSubscriber implements EventSubscriberInterface
                 array('installAcmeDemoBundle', 0)
             ),
         );
+    }
+
+    protected static function getOptions(CommandEvent $event)
+    {
+        $options = array_merge(self::$options, $event->getComposer()->getPackage()->getExtra());
+
+        return $options;
     }
 }
